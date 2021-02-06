505b2b2e67e1937110328c9f4ceada713c8c919e
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-06 17:58:29 +1300

    fetch data from backend

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Product from "../Product";
-import { data } from "../../data";
 
 function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="row center">
-      {data.products.map((product) => (
+      {products.map((product) => (
         <Product key={product._id} product={product} />
       ))}
     </div>
