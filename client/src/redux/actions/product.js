d228341754c20b0d40e0fe64e03b544890907354
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-07 10:44:14 +1300

    add fetchProductDetail

--- client/src/redux/actions/product.js
+++ client/src/redux/actions/product.js
@@ -3,6 +3,9 @@ import {
   PRODUCTS_FAIL,
   PRODUCTS_REQUEST,
   PRODUCTS_SUCCESS,
+  PRODUCT_DETAIL_FAIL,
+  PRODUCT_DETAIL_REQUEST,
+  PRODUCT_DETAIL_SUCCESS,
 } from "../actionTypes";
 
 export const fetchProducts = () => async (dispatch) => {
@@ -15,5 +18,24 @@ export const fetchProducts = () => async (dispatch) => {
     dispatch({ type: PRODUCTS_SUCCESS, payload: data });
   } catch (error) {
     dispatch({ type: PRODUCTS_FAIL, payload: error.message });
+  }
+};
+
+export const fetchProductDetail = (productId) => async (dispatch) => {
+  dispatch({
+    type: PRODUCT_DETAIL_REQUEST,
+  });
+
+  try {
+    const { data } = await axios.get(`/api/products/${productId}`);
+    dispatch({ type: PRODUCT_DETAIL_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAIL_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
   }
 };
