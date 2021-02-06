1b2d82b895f7f98349a84d293ddc1d1ceb5748eb
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-07 10:44:47 +1300

    add product detail reducer to store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import products from "./reducers/products";
+import productDetail from "./reducers/productDetail";
 
 const initState = {};
 const reducer = combineReducers({
   products,
+  productDetail,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
