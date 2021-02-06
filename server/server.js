ac468e5a144600999db430bd8da8176563505b4f
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-07 10:46:07 +1300

    add product detail api

--- server/server.js
+++ server/server.js
@@ -4,9 +4,24 @@ import { data } from "./data.js";
 
 const app = express();
 
+// fetch the specific product detail
+app.get("/api/products/:id", (req, res) => {
+  const id = req.params.id;
+  const product = data.products.find((p) => p._id === id);
+
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not Found" });
+  }
+});
+
+//fetch all products
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
+
+// test api
 app.get("/", (req, res) => {
   res.send("Server is ready.");
 });
