6f6ae730a4ea4d397e5d233cb08c6d1a8bee4cd4
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-06 17:40:57 +1300

    move data file from client to server and create api

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 import express from "express";
 
+import { data } from "./data.js";
+
 const app = express();
 
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
 app.get("/", (req, res) => {
   res.send("Server is ready.");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000.");
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
 });
