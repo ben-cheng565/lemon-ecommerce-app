4990c3092ffe649b3cb212e462885d6c5a052877
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-07 21:41:54 +1300

    create cart total component

---
+++ client/src/components/cart/cartTotal/CartTotal.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "./CartTotal.css";
+
+function CartTotal(props) {
+  const { cartItems, checkoutHandler } = props;
+
+  return (
+    <div className="card card-body">
+      <ul>
+        <li>
+          <h2>
+            Subtotal ({cartItems.reduce((accum, c) => accum + c.qty, 0)}, items)
+            : $ {cartItems.reduce((accum, c) => accum + c.price * c.qty, 0)}
+          </h2>
+        </li>
+        <li>
+          <button
+            type="button"
+            onClick={checkoutHandler}
+            disabled={cartItems.length === 0}
+          >
+            Check Out
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default CartTotal;
