6b65b9c96841136d4eee7a88123d8ba06a7e2964
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-08 10:01:52 +1300

    split file into several components

--- client/src/components/product/detail/ProductDetail.js
+++ client/src/components/product/detail/ProductDetail.js
@@ -7,6 +7,8 @@ import Rating from "../../common/Rating";
 import { fetchProductDetail } from "../../../redux/actions/product";
 
 import "./ProductDetail.css";
+import ProductContent from "./productContent/ProductContent";
+import AddToCart from "./addToCart/AddToCart";
 
 function ProductDetail(props) {
   // const product = data.products.find((p) => p._id === props.match.params.id);
@@ -34,78 +36,14 @@ function ProductDetail(props) {
         <div>
           <Link to="/">Go Back</Link>
           <div className="row top">
-            <div className="col-2">
-              <img
-                className="large"
-                src={product.image}
-                alt={product.name}
-              ></img>
-            </div>
-            <div className="col-1">
-              <ul>
-                <li>
-                  <h1>{product.name}</h1>
-                </li>
-                <li>
-                  <Rating
-                    rating={product.rating}
-                    numReviews={product.numReviews}
-                  />
-                </li>
-                <li>Price : ${product.price}</li>
-                <li>Description : {product.description}</li>
-              </ul>
-            </div>
-            <div className="col-1">
-              <div className="card card-body">
-                <ul>
-                  <li>
-                    <div className="row">
-                      <div>Price</div>
-                      <div className="price">${product.price}</div>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="row">
-                      <div>Status</div>
-                      <div>
-                        {product.countInStock > 0 ? (
-                          <span className="success">In Stock</span>
-                        ) : (
-                          <span className="error">Out of Stock</span>
-                        )}
-                      </div>
-                    </div>
-                  </li>
-                  {product.countInStock > 0 && (
-                    <>
-                      <li>
-                        <div className="row">
-                          <div>Qty</div>
-                          <div>
-                            <select
-                              value={qty}
-                              onChange={(e) => setQty(e.target.value)}
-                            >
-                              {[...Array(product.countInStock).keys()].map(
-                                (x) => (
-                                  <option key={x + 1} value={x + 1}>
-                                    {x + 1}
-                                  </option>
-                                )
-                              )}
-                            </select>
-                          </div>
-                        </div>
-                      </li>
-                      <li>
-                        <button onClick={addToCartHandler}>Add to Cart</button>
-                      </li>
-                    </>
-                  )}
-                </ul>
-              </div>
-            </div>
+            <ProductContent product={product} />
+
+            <AddToCart
+              product={product}
+              qty={qty}
+              setQty={setQty}
+              addToCartHandler={addToCartHandler}
+            />
           </div>
         </div>
       )}
