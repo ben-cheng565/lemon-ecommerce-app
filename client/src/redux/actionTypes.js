25077b71c9003e246a33a0ef4cc44cbe95de3f07
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-07 15:58:16 +1300

    add cart action type

--- client/src/redux/actionTypes.js
+++ client/src/redux/actionTypes.js
@@ -1,3 +1,4 @@
+// products action types
 export const PRODUCTS_REQUEST = "PRODUCTS_REQUEST";
 export const PRODUCTS_SUCCESS = "PRODUCTS_SUCCESS";
 export const PRODUCTS_FAIL = "PRODUCTS_FAIL";
@@ -5,3 +6,6 @@ export const PRODUCTS_FAIL = "PRODUCTS_FAIL";
 export const PRODUCT_DETAIL_REQUEST = "PRODUCT_DETAIL_REQUEST";
 export const PRODUCT_DETAIL_SUCCESS = "PRODUCT_DETAIL_SUCCESS";
 export const PRODUCT_DETAIL_FAIL = "PRODUCT_DETAIL_FAIL";
+
+// cart action types
+export const CART_ADD_ITEM = "CART_ADD_ITEM";
