b5d4480092b0efcf632e0592337769dd84e2d144
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-07 15:59:27 +1300

    update cart page to fetch data from backend

--- client/src/components/pages/Cart.js
+++ client/src/components/pages/Cart.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/actions/cart";
 
 function Cart(props) {
+  const dispatch = useDispatch();
   const productId = props.match.params.id;
   const qty = props.location.search
     ? Number(props.location.search.split("=")[1])
     : 1;
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
   return (
     <div>
       {productId} : {qty}
