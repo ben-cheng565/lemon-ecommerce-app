17c9bf577199f2e9885bb423373fc54b64389785
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-07 21:42:07 +1300

    create cartItem component

---
+++ client/src/components/cart/cartItem/CartItem.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+
+import { addToCart } from "../../../redux/actions/cart";
+import "./CartItem.css";
+
+function CartItem(props) {
+  const dispatch = useDispatch();
+  const { item, removeFromCartHandler } = props;
+
+  return (
+    <li key={item.id}>
+      <div className="row">
+        <div>
+          <img src={item.image} alt={item.name} className="small" />
+        </div>
+        <div className="min-30">
+          <Link to={`/product/${item.id}`}>{item.name}</Link>
+        </div>
+        <div>
+          <select
+            value={item.qty}
+            onChange={(e) =>
+              dispatch(addToCart(item.id, Number(e.target.value)))
+            }
+          >
+            {[...Array(item.countInStock).keys()].map((x) => (
+              <option key={x + 1} value={x + 1}>
+                {x + 1}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>${item.price}</div>
+        <div>
+          <button
+            className="delete"
+            type="button"
+            onClick={() => removeFromCartHandler(item.id)}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default CartItem;
