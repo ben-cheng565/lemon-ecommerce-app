354838fd7edf9054a4272bcba829dedfd4e06933
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-08 13:10:36 +1300

    add products init api

--- server/routers/products.js
+++ server/routers/products.js
@@ -1,13 +1,25 @@
 import express from "express";
+import Product from "../models/products.js";
 
 import { data } from "../data.js";
+import Products from "../models/products.js";
 
 const router = express.Router();
 
+// init products data
+router.get("/init", async (req, res) => {
+  // before initializing data, delete all product data in db
+  await Product.remove({});
+
+  const createdProducts = await Product.insertMany(data.products);
+
+  res.send({ createdProducts });
+});
+
 // fetch the specific product detail
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   const id = req.params.id;
-  const product = data.products.find((p) => p._id === id);
+  const product = await Products.findById(id);
 
   if (product) {
     res.send(product);
@@ -17,8 +29,9 @@ router.get("/:id", (req, res) => {
 });
 
 //fetch all products
-router.get("/", (req, res) => {
-  res.send(data.products);
+router.get("/", async (req, res) => {
+  const products = await Products.find({});
+  res.send(products);
 });
 
 export default router;
