7e7c47f02647fd500fd32bcc2fe0184bd5de9f32
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-08 13:42:49 +1300

    add signin api

--- server/routers/users.js
+++ server/routers/users.js
@@ -1,6 +1,8 @@
 import express from "express";
+import bcrypt from "bcryptjs";
 import { data } from "../data.js";
 import User from "../models/users.js";
+import { generateToken } from "../util.js";
 
 const router = express.Router();
 
@@ -13,4 +15,27 @@ router.get("/init", async (req, res) => {
   res.send({ createdUsers });
 });
 
+// sign in api
+router.post("/signin", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    res.status(401).send({ message: "Invalid email address" });
+    return;
+  }
+
+  const isEqual = await bcrypt.compare(req.body.password, user.password);
+  if (!isEqual) {
+    res.status(401).send({ message: "Invalid password" });
+    return;
+  }
+
+  res.send({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin,
+    token: generateToken(user),
+  });
+});
 export default router;
