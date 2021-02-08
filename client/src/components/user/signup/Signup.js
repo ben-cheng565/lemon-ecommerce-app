f6b31340d4182ae67d92e78635a1946453f529f6
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-09 10:31:57 +1300

    create sign up component

---
+++ client/src/components/user/signup/Signup.js
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { signIn, signUp } from "../../../redux/actions/user";
+import LoadingBox from "../../common/LoadingBox";
+import MessageBox from "../../common/MessageBox";
+
+import "./Signup.css";
+
+function Signin(props) {
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  //   extract redirect page from url
+  const redirect = props.location.search
+    ? props.location.search.split("=")[1]
+    : "/";
+
+  const { userInfo, loading, error } = useSelector((state) => state.userSignin);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("Password and confirm passwrod are not match.");
+    } else {
+      dispatch(signUp(name, email, password));
+    }
+  };
+
+  useEffect(() => {
+    if (userInfo) {
+      props.history.push(redirect);
+    }
+  }, [props.history, redirect, userInfo]);
+
+  return (
+    <div>
+      <form className="form" onSubmit={submitHandler}>
+        <div>
+          <h1>Create Account</h1>
+        </div>
+        {loading && <LoadingBox />}
+        {error && <MessageBox variant="danger">{error}</MessageBox>}
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="name"
+            id="name"
+            placeholder="Enter name"
+            required
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="email">Email Adress</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            placeholder="Enter Confirm password"
+            required
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label />
+          <button type="submit">Sign Up</button>
+        </div>
+        <div>
+          <label />
+          <div>
+            Already have an account?{" "}
+            <Link to={`/signin?redirect=${redirect}`}>Sign In</Link>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Signin;
