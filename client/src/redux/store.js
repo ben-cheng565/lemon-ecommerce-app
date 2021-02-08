4dfe2c384ef1f44d19acc6c5c11c081b85973f01
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-09 10:32:29 +1300

    add sign up reducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,8 +3,16 @@ import thunk from "redux-thunk";
 import products from "./reducers/products";
 import productDetail from "./reducers/productDetail";
 import cart from "./reducers/cart";
+import { userSignin, userSignup } from "./reducers/user";
 
 const initState = {
+  // read user signin data from the local storage
+  userSignin: {
+    userInfo: localStorage.getItem("userInfo")
+      ? JSON.parse(localStorage.getItem("userInfo"))
+      : null,
+  },
+
   // read cart data from the local storage
   cart: {
     cartItems: localStorage.getItem("cartItems")
@@ -16,6 +24,8 @@ const reducer = combineReducers({
   products,
   productDetail,
   cart,
+  userSignin,
+  userSignup,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
