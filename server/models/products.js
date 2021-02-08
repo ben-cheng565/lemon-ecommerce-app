5ff2ed2489ad75b4431faaea82f2b56354565ff3
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-08 13:10:19 +1300

    create products model

---
+++ server/models/products.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const productsSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    image: { type: String },
+    brand: { type: String, required: true },
+    category: { type: String, required: true },
+    description: { type: String },
+    price: { type: Number, required: true },
+    countInStock: { type: Number, default: 0 },
+    rating: { type: Number },
+    numReviews: { type: Number },
+  },
+  { timestamps: true }
+);
+
+const Products = mongoose.model("Products", productsSchema);
+export default Products;
