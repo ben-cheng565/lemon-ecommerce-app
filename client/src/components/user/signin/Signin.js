e876630981f193e24b3427a1296b6d197c113537
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-08 18:28:04 +1300

    make siginin button work

--- client/src/components/user/signin/Signin.js
+++ client/src/components/user/signin/Signin.js
@@ -1,21 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { signIn } from "../../../redux/actions/user";
+import LoadingBox from "../../common/LoadingBox";
+import MessageBox from "../../common/MessageBox";
 
 import "./Signin.css";
 
-function Signin() {
+function Signin(props) {
+  const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  //   extract redirect page from url
+  const redirect = props.location.search
+    ? props.location.search.split("=")[1]
+    : "/";
+
+  const { userInfo, loading, error } = useSelector((state) => state.userSignin);
+
   const submitHandler = (e) => {
     e.preventDefault();
+
+    dispatch(signIn(email, password));
   };
+
+  useEffect(() => {
+    if (userInfo) {
+      props.history.push(redirect);
+    }
+  }, [props.history, redirect, userInfo]);
+
   return (
     <div>
       <form className="form" onSubmit={submitHandler}>
         <div>
           <h1>Sign In</h1>
         </div>
+        {loading && <LoadingBox />}
+        {error && <MessageBox variant="danger">{error}</MessageBox>}
         <div>
           <label htmlFor="email">Email Adress</label>
           <input
@@ -23,7 +46,6 @@ function Signin() {
             id="email"
             placeholder="Enter email"
             required
-            value={email}
             onChange={(e) => setEmail(e.target.value)}
           ></input>
         </div>
@@ -34,7 +56,6 @@ function Signin() {
             id="password"
             placeholder="Enter password"
             required
-            value={password}
             onChange={(e) => setPassword(e.target.value)}
           ></input>
         </div>
