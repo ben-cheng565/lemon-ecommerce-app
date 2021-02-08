1d5710a6d4471f3307123f1b753ed7c4d7b0bbf7
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-08 16:21:44 +1300

    create sign in component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Cart from "./components/cart/Cart";
 import Home from "./pages/Home";
 import ProductDetail from "./components/product/detail/ProductDetail";
 import CartBadge from "./components/cart/badge/CartBadge";
+import Signin from "./components/user/signin/Signin";
 
 function App() {
   return (
@@ -24,8 +25,9 @@ function App() {
         </header>
         <main>
           <Route path="/cart/:id?" component={Cart} />
-          <Route path="/" exact component={Home} />
+          <Route path="/signin" component={Signin} />
           <Route path="/product/:id" component={ProductDetail} />
+          <Route path="/" exact component={Home} />
         </main>
         <footer className="row center">All right reserved</footer>
       </div>
