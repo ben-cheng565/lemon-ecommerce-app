31d68efe6542a924abf174158d6746951796e73e
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-09 19:46:04 +1300

    add isAuth middleware

--- server/util.js
+++ server/util.js
@@ -12,3 +12,20 @@ export const generateToken = (user) => {
     { expiresIn: "1h" }
   );
 };
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SECRET || "privatekey", (err, decode) => {
+      if (err) {
+        res.status(401).send({ message: "Invalid token." });
+      } else {
+        req.userInfo = decode;
+        next();
+      }
+    });
+  } else {
+    res.status(401).send({ message: "No token." });
+  }
+};
