fa98739f5370cc772c40a74b563e2ff31ee08149
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-10 14:37:10 +1300

    create order details page

---
+++ client/src/pages/order/OrderDetails.js
@@ -0,0 +1,137 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import LoadingBox from "../../components/common/LoadingBox";
+import MessageBox from "../../components/common/MessageBox";
+
+import { getOrderDetails } from "../../redux/actions/order";
+
+function OrderDetails(props) {
+  const orderId = props.match.params.id;
+  const { order, loading, error } = useSelector((state) => state.orderDetails);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getOrderDetails(orderId));
+  }, [dispatch, orderId]);
+
+  return loading ? (
+    <LoadingBox />
+  ) : error ? (
+    <MessageBox variant="danger" />
+  ) : (
+    <div>
+      <h1>Order: {order._id}</h1>
+      <div className="row top">
+        <div className="col-2">
+          <ul>
+            <li>
+              <div className="card card-body">
+                <h2>Shipping</h2>
+                <p>
+                  <strong>Name: </strong>
+                  {order.shippingAddress.fullName} <br />
+                  <strong>Address: </strong>
+                  {order.shippingAddress.address}, {order.shippingAddress.city},
+                  {order.shippingAddress.country},{" "}
+                  {order.shippingAddress.postalCode}
+                </p>
+                {order.isDelivered ? (
+                  <MessageBox variant="success">
+                    Delivered at {order.deliveredAt}
+                  </MessageBox>
+                ) : (
+                  <MessageBox variant="danger">
+                    Have not delivered yet.
+                  </MessageBox>
+                )}
+              </div>
+            </li>
+            <li>
+              <div className="card card-body">
+                <h2>Payment</h2>
+                <p>
+                  <strong>Method: </strong>
+                  {order.paymentMethod}
+                </p>
+                {order.isPaid ? (
+                  <MessageBox variant="success">
+                    Paid at {order.paidAt}
+                  </MessageBox>
+                ) : (
+                  <MessageBox variant="danger">Have not paid yet.</MessageBox>
+                )}
+              </div>
+            </li>
+            <li>
+              <div className="card card-body">
+                <h2>Order Items</h2>
+                <ul>
+                  {order.orderItems.map((item) => (
+                    <li key={item.productId}>
+                      <div className="row">
+                        <div>
+                          <img
+                            src={item.image}
+                            alt={item.name}
+                            className="small"
+                          />
+                        </div>
+                        <div className="min-30">
+                          <Link to={`/product/${item.id}`}>{item.name}</Link>
+                        </div>
+
+                        <div>
+                          {item.qty} x ${item.price} = ${item.qty * item.price}
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="col-1">
+          <div className="card card-body">
+            <ul>
+              <li>
+                <h2>Order Summary</h2>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Items</div>
+                  <div>${order.itemsPrice.toFixed(2)}</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Shipping</div>
+                  <div>${order.shippingPrice.toFixed(2)}</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Tax</div>
+                  <div>${order.taxPrice.toFixed(2)}</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div>
+                    <strong>Order Total</strong>
+                  </div>
+                  <div>
+                    <strong>${order.totalPrice.toFixed(2)}</strong>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default OrderDetails;
