73d1d8f8ad7c1ce8a35a12c0f8b12c50887052dc
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-17 19:38:03 +1300

    add admin menu

--- client/src/components/navBar/NavBar.js
+++ client/src/components/navBar/NavBar.js
@@ -13,6 +13,7 @@ function NavBar() {
   const { userInfo } = useSelector((state) => state.user);
   const { cartItems } = useSelector((state) => state.cart);
 
+  console.log(userInfo);
   const signoutHandler = () => {
     //   sign out user account
     dispatch(signout());
@@ -59,6 +60,27 @@ function NavBar() {
           </div>
         ) : (
           <Link to="/signin">Sign In</Link>
+        )}
+        {userInfo && userInfo.isAdmin && (
+          <div className="dropdown">
+            <Link to="#Admin">
+              Admin <i className="fa fa-caret-down"></i>
+            </Link>
+            <ul className="dropdown-content">
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <Link to="/productlist">Products</Link>
+              </li>
+              <li>
+                <Link to="/orderlist">Orders</Link>
+              </li>
+              <li>
+                <Link to="/userlist">Users</Link>
+              </li>
+            </ul>
+          </div>
         )}
       </div>
     </>
