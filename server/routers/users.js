f32d39d59ed2bc327b99baa41c5d2c51c52efb12
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-17 18:52:03 +1300

    create update profile api

--- server/routers/users.js
+++ server/routers/users.js
@@ -2,7 +2,7 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import { data } from "../data.js";
 import User from "../models/users.js";
-import { generateToken } from "../util.js";
+import { generateToken, isAuth } from "../util.js";
 
 const router = express.Router();
 
@@ -62,6 +62,26 @@ router.get("/:id", async (req, res) => {
     res.send(user);
   } else {
     res.status(404).send({ message: "User not Found" });
+  }
+});
+
+router.put("/profile", isAuth, async (req, res) => {
+  const user = await User.findById(req.userInfo._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = await bcrypt.hash(req.body.password, 10);
+    }
+
+    const result = await user.save();
+    res.send({
+      _id: result._id,
+      name: result.name,
+      email: result.email,
+      isAdmin: result.isAdmin,
+      token: generateToken(result),
+    });
   }
 });
 
