f91bb10b03671124363fe936232fc171152a022c
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-18 20:54:20 +1300

    fix bug

--- server/util.js
+++ server/util.js
@@ -31,7 +31,7 @@ export const isAuth = (req, res, next) => {
 };
 
 export const isAdmin = (req, res, next) => {
-  if (req.user && req.user.isAdmin) {
+  if (req.userInfo && req.userInfo.isAdmin) {
     next();
   } else {
     res.status(401).send({ message: "Invalid admin token." });
