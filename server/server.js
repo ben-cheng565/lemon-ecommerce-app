9a2aa1a86a092d3fee79fe6bcad50997e064cae8
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-22 13:28:38 +1300

    commit

--- server/server.js
+++ server/server.js
@@ -44,10 +44,10 @@ app.get("/config/paypal", (req, res) => {
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-app.use(express.static(path.join(__dirname, '/client/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/client/build/index.html'))
-
+app.use(express.static(path.join(__dirname, "/client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
