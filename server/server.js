9cea58b0773bea6696dab4e36b1abc5d18b242be
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-24 13:43:23 +1300

    fix image problem

--- server/server.js
+++ server/server.js
@@ -43,7 +43,8 @@ app.get("/config/paypal", (req, res) => {
 });
 
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+// app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use("/uploads", express.static("uploads"));
 app.use(express.static(path.join(__dirname, "/client/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/client/build/index.html"));
