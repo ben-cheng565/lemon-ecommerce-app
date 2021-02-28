4e4fc8d074c1f7006d802a1246bfb2b58e960a90
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-02-28 15:19:41 +1300

    create search box component

---
+++ client/src/components/search/SearchBox.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+import "./SearchBox.css";
+
+function SearchBox(props) {
+  const [name, setName] = useState("all");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    props.history.push(`/search?name=${name}`);
+  };
+
+  return (
+    <div>
+      <form className="search" onSubmit={submitHandler}>
+        <div className="row">
+          <input
+            type="text"
+            name="keyWords"
+            id="keyWords"
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+          <button>
+            <i className="fa fa-search"></i>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default SearchBox;
