fedd6d5ab33cba2bba700041e2fdf1be49645f5e
ben-cheng565 <55331180+ben-cheng565@users.noreply.github.com>
2021-03-15 22:26:29 +1300

    adjust navbar style

--- client/src/components/navBar/NavBar.js
+++ client/src/components/navBar/NavBar.js
@@ -31,9 +31,9 @@ function NavBar() {
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar scroll
-          </a>
+          <Link className="navbar-brand" to="/">
+            Dandelion e-shop
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -43,19 +43,89 @@ function NavBar() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarScroll">
-            <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+            <ul className="navbar-nav navbar-nav-scroll ms-auto">
+              {userInfo ? (
+                <li class="nav-item dropdown">
+                  <Link
+                    class="nav-link dropdown-toggle active"
+                    to="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                  >
+                    Hello, {userInfo.name}
+                  </Link>
+                  {!userInfo.isAdmin && (
+                    <ul class="dropdown-menu me-auto mb-2 dropdown-menu-dark">
+                      <li>
+                        <Link className="dropdown-item" to="/profile">
+                          <i class="fas fa-user-circle"></i> Profile
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item" to="/orderhistory">
+                          <i class="fas fa-history"></i> Orders
+                        </Link>
+                      </li>
+                      <li>
+                        <hr class="dropdown-divider" />
+                      </li>
+                      <li>
+                        <Link
+                          className="dropdown-item"
+                          to="/"
+                          onClick={signoutHandler}
+                        >
+                          <i class="fas fa-sign-out-alt"></i> Sign Out
+                        </Link>
+                      </li>
+                    </ul>
+                  )}
+                  {userInfo.isAdmin && (
+                    <ul class="dropdown-menu me-auto mb-2 dropdown-menu-dark">
+                      <li>
+                        <Link className="dropdown-item" to="/productlist">
+                          <i class="fas fa-boxes"></i> Products
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item" to="/orderlist">
+                          <i class="fas fa-history"></i> Orders
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item" to="/userlist">
+                          <i class="fas fa-users-cog"></i> Users
+                        </Link>
+                      </li>
+                      <li>
+                        <hr class="dropdown-divider" />
+                      </li>
+                      <li>
+                        <Link
+                          className="dropdown-item"
+                          to="/"
+                          onClick={signoutHandler}
+                        >
+                          <i class="fas fa-sign-out-alt"></i> Sign Out
+                        </Link>
+                      </li>
+                    </ul>
+                  )}
+                </li>
+              ) : (
+                <Link className="nav-link active" to="/signin">
+                  Sign In
+                </Link>
+              )}
               <li className="nav-item">
-                <a className="nav-link active" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
+                <Link to="/cart" className="nav-link active">
+                  <i class="fas fa-shopping-cart"></i> Cart
+                  <CartBadge cartItems={cartItems} />
+                </Link>
               </li>
             </ul>
-            <form className="d-flex">
+            {/* <form className="d-flex">
               <input
                 className="form-control me-2"
                 type="search"
@@ -64,7 +134,7 @@ function NavBar() {
               <button className="btn btn-outline-light" type="submit">
                 Search
               </button>
-            </form>
+            </form> */}
           </div>
         </div>
       </nav>
